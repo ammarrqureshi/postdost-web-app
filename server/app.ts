d303f0d399ee56824fc6e9be0a81b0f60d1e09ef
Mayar <mayar@mayars-air.mynet>
2023-04-10 22:52:08 +0300

    connect with mongoDB

--- server/app.ts
+++ server/app.ts
@@ -2,38 +2,69 @@ import express from "express";
 import cors from "cors";
 import { join } from "path";
 import dotenv from "dotenv";
+import session from "express-session";
+import { dbConnection } from "./database";
+import CustomError from "./errors/CustomError";
+const MongoDBStore = require("connect-mongodb-session")(session);
 
+// Load environment variables from file
 dotenv.config();
 
+// Destructure environment variables
+const {
+  NODE_ENV = "development",
+  PORT = 3000,
+  MONGODB_SESSION_URI,
+  SESSION_SECRET_KEY,
+} = process.env;
+
+// Create an instance of the Express application
 const app = express();
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || "development";
 
-app.set("port", port );
-app.set("env", env );
+// Set the application settings
+app.set("port", PORT);
+app.set("env", NODE_ENV);
 
+// Configure middleware for handling JSON and form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-if (env === "production") {
-  // app.use(cors({ origin: "https://example.com" }));
-} else {
-  app.use(cors());
-}
+// Establish database connection
+dbConnection();
 
-if (env === "development") {
+
+const store = new MongoDBStore({
+  uri: MONGODB_SESSION_URI,
+  collection: "mySessions",
+});
+if (!SESSION_SECRET_KEY) {
+  throw new CustomError(
+    "SESSION_SECRET_KEY is not defined",
+    500,
+    "SessionSecretKeyNotFoundError"
+  );
+}
+app.use(
+  session({
+    secret: SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+if (NODE_ENV === "development") {
+  app.use(cors());
   app.get("/", (req, res) => {
     res.json({ message: "Server Is Running" });
   });
-}
-
-if (env === "production") {
+} else if (NODE_ENV === "production") {
+  // app.use(cors({ origin: "https://example.com" }));
+  // Serve static files and fallback to index.html for production environment
   app.use(express.static(join(__dirname, "..", "build")));
   app.get("*", (req, res) => {
     res.sendFile(join(__dirname, "..", "build", "index.html"));
   });
 }
-
-
 
 export default app;
