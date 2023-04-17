1d0c5ba71d7ad769d35f94d94ae7ad4c93c993d3
ammarrqureshi <mamqureshi786@gmail.com>
2023-04-17 21:34:09 +0500

    first commit after rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './App.css'
 import Influencer from './components/pages/Influencer/Influencer';
 
@@ -10,6 +11,8 @@ function App() {
   
   export default App;
 =======
+=======
+>>>>>>> 08c1caf (first commit after rebase)
 import Explore from './components/pages/Explore/Explore';
 import ExploreProvider from './contexts/ExploreContext';
 import SignUp from './components/pages/SignUp/SignUp';
@@ -28,4 +31,44 @@ function App() {
   );
 }
 export default App;
+<<<<<<< HEAD
 >>>>>>> dc05564 (Explore Page)
+=======
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
+>>>>>>> 24d1d62 (created react project)
+>>>>>>> 08c1caf (first commit after rebase)
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import './index.css';
 import App from './App';
 =======
@@ -8,6 +9,15 @@ import './index.css'
 import App from './App'
 import './index.css';
 >>>>>>> dc05564 (Explore Page)
+=======
+import './index.css'
+import App from './App'
+import './index.css';
+=======
+import App from './App'
+import './index.css'
+>>>>>>> 24d1d62 (created react project)
+>>>>>>> 08c1caf (first commit after rebase)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
