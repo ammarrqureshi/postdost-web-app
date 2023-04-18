d214e3a455c1da67667aac9e0c48f194b521a1aa
Ammar Qureshi <mamqureshi786@gmail.com>
2023-04-18 20:40:52 +0500

    Update globalErrors.middleware.ts

--- server/middleware/globalErrors.middleware.ts
+++ server/middleware/globalErrors.middleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import CustomError from "../errors/CustomError";
-
 export default function globalErrors(
   err: Error,
   req: Request,
@@ -10,21 +9,6 @@ export default function globalErrors(
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json(err.toJSON());
   }
-
   return res
     .status(500)
     .json(new CustomError(err.message, 500, err.name).toJSON());
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  // return res.status(500).json({
-  //   error: {
-  //     name: "ServerError",
-  //     message: `Internal server error :{}`,
-  //     statusCode: 500,
-  //   },
-  // });
->>>>>>> a7c9aa0 (create globalErrors & unhandleRoutes  middleware)
-=======
->>>>>>> 8ff5784 (fix  globalErrors  middleware)
-}
